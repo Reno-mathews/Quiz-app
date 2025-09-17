eb7a5b60b32190a6036de28cbbcf95739fedeaf9
Reno-mathews <renomathews10pro@gmail.com>
2025-09-17 18:47:38 +0100

    Updated Jaavascript

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ let questions = [
     { question: "What is 4+4", options: [1, 2, 7, 8], answer: 8}
 ];
 
-function qselect(question) {
-    question[i]
+function loadQuestion(index) {
+    let q = questions[index];
+
+    document.getElementById("question").innerText = q.question;
+
+    document.getElementById("opt1").innerText = q.options[0];
+    document.getElementById("opt2").innerText = q.options[1];
+    document.getElementById("opt3").innerText = q.options[2];
+    document.getElementById("opt3").innerText = q.options[3];
+
 }
+
+loadQuestion(0);
