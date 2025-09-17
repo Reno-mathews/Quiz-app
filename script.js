3d0e6055614e5ab888a39a1d25ea88c9fff7d018
Reno-mathews <renomathews10pro@gmail.com>
2025-09-17 18:32:07 +0100

    Updated Jaavascript

---
+++ script.js
@@ -0,0 +1,9 @@
+let questions = [
+    { question: "What is 2+2", options: [1, 2, 3, 4], answer: 4},
+    { question: "What is 3+3", options: [1, 2, 5, 6], answer: 6},
+    { question: "What is 4+4", options: [1, 2, 7, 8], answer: 8}
+];
+
+function qselect(question) {
+    question[i]
+}
