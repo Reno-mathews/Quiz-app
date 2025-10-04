00c7d828e9207573c00593135d0aeabc4570d243
Reno-mathews <renomathews10pro@gmail.com>
2025-10-04 18:08:44 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ import psycopg2
 from flask_cors import CORS
 
 
+
 app= Flask(__name__)
 app.secret_key = "quiz_app"
-
+CORS(app)
 
 conn = psycopg2.connect(
     host ="localhost",
@@ -16,7 +17,9 @@ conn = psycopg2.connect(
     password = "Halosucks10@"
 )
 
-cur = conn.cursor()
+cur = conn.cursor()                                                                                                                                                                                                                         
 
 @app.route('/get_questions')
-def
+def g_question():
+    cur.execute("SELECT question_text, option1, option2, option3, option4, correct_answer, difficulty, category FROM quiz_data")
+    questions = cur.fetchall()
