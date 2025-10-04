48bcff9036020f8a65d53c2257ae856a03af6f28
Reno-mathews <renomathews10pro@gmail.com>
2025-10-04 16:12:40 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ import psycopg2
 
 app= Flask(__name__)
 app.secret_key = "quiz_app"
+
+
+conn = psycopg2.connect(
+    host ="localhost",
+    database = "quiz_data"
+    user = "postgres",
+    password = "Halosucks10@"
+)
+
+cur = conn.cursor()
